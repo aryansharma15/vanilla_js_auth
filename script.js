1c199e34ead91596691793984c19b4bd5dbb9f40
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-07-23 22:27:49 +0530

    configure client function

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 import { createAuth0Client } from "@auth0/auth0-spa-js";
 
+let auth0Client = null;
+
 const regForm = document.getElementById("reg-form");
 const loginForm = document.getElementById("login-form");
+
+// fetching the auth_config file
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const configureClient = async () => {
+	const response = await fetchAuthConfig();
+	const config = await response.json();
+	auth0Client = await auth0.createAuth0Client({
+		domain: config.domain,
+		clientId: config.clientId,
+	});
+};
 
 //
 // Implementing registering functionality for new users
--- server.js
+++ server.js
@@ -3,4 +3,19 @@ const { join } = require("path");
 
 const app = express();
 
+// Serving static files from the directory
+// There is no public directory, change if there's an error
 app.use(express.static(join(__dirname, "public")));
+
+// Endpoint to serve the configuration file
+app.get("/auth_config.json", (req, res) => {
+	res.sendFile(join(__dirname, "auth_config.json"));
+});
+
+// Serve the index page for all other requests
+app.get("/*", (_, res) => {
+	res.sendFile(join(__dirname, "index.html"));
+});
+
+// Listen on port 3000
+app.listen(3000, () => console.log("Application running on port 3000"));
