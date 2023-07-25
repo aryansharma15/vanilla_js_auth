e29bf691534fe08235ef5e270acc8bb5f66ca323
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-07-25 18:43:35 +0530

    login unsuccessful

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-//
-// const express = require("express");
-// const { join } = require("path");
-
-// const app = express();
-
-// // Serving static files from the directory
-// // There is no public directory, change if there's an error
-// app.use(express.static(join(__dirname, "public")));
-
-// // Endpoint to serve the configuration file
-// app.get("/auth_config.json", (req, res) => {
-// 	res.sendFile(join(__dirname, "auth_config.json"));
-// });
-
-// // Serve the index page for all other requests
-// app.get("/*", (_, res) => {
-// 	res.sendFile(join(__dirname, "index.html"));
-// });
-
-// // Listen on port 3000
-// app.listen(3000, () => console.log("Application running on port 3000"));
-
 const { auth } = require("express-openid-connect");
 
 const config = {
@@ -39,10 +16,3 @@ app.use(auth(config));
 app.get("/", (req, res) => {
 	res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
-
-// middleware
-// const { requiresAuth } = require("express-openid-connect");
-
-// app.get("/profile", requiresAuth(), (req, res) => {
-// 	res.send(JSON.stringify(req.oidc.user));
-// });
