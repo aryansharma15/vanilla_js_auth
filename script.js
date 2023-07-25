c0092d0a8ef1c8fb63193d24e7f8fe76352e5a2a
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-07-25 18:51:46 +0530

    rewriting scriptjs

--- script.js
+++ script.js
@@ -1,103 +1,18 @@
-import { createAuth0Client } from "@auth0/auth0-spa-js";
+const { auth } = require("express-openid-connect");
 
-let auth0Client = null;
-
-const regForm = document.getElementById("reg-form");
-const loginForm = document.getElementById("login-form");
-
-// fetching the auth_config file
-const fetchAuthConfig = () => fetch("/auth_config.json");
-
-// downloads the configuration file and initialize the "auth0Client"
-const configureClient = async () => {
-	const response = await fetchAuthConfig();
-	const config = await response.json();
-	auth0Client = await auth0.createAuth0Client({
-		domain: config.domain,
-		clientId: config.clientId,
-	});
+const config = {
+	authRequired: false,
+	auth0Logout: true,
+	secret: "a long, randomly-generated string stored in env",
+	baseURL: "https://127.0.0.1:5500",
+	clientID: "85eOHVFdxg8HPMuhhAuTjFpgbzxzu33O",
+	issuerBaseURL: "https://dev-oo7j7v2fgym2imov.us.auth0.com",
 };
 
-window.onload = async () => {
-	await configureClient();
-};
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
-//
-// Implementing registering functionality for new users
-
-regForm.addEventListener("submit", function (event) {
-	event.preventDefault();
-
-	const name = document.getElementById("reg-name");
-	const email = document.getElementById("reg-email");
-	const uname = document.getElementById("reg-uname");
-	const password = document.getElementById("reg-pass");
-
-	registerUser(name, email, uname, password);
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+	res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
-
-function registerUser(name, email, uname, password) {
-	const userData = {
-		name: name,
-		email: email,
-		username: uname,
-		password: password,
-	};
-
-	fetch("", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(userData),
-	})
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-		})
-
-		.catch((error) => {
-			console.error("Error:", error);
-		});
-}
-
-//
-// Implementing login functionality
-
-loginForm.addEventListener("submit", function (event) {
-	event.preventDefault();
-	const uname = document.getElementById("login-uname");
-	const pass = document.getElementById("login-pass");
-
-	loginUser(uname, pass);
-});
-
-function loginUser(username, password) {
-	const userData = {
-		username: username,
-		password: password,
-	};
-
-	fetch("", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(userData),
-	})
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-		})
-		.catch((error) => {
-			console.error("Error: ", error);
-		});
-}
-
-// Logging out method
-
-function logoutUser() {
-	localStorage.removeItem("jwtToken");
-
-	window.location.href = "/regis.html";
-}
