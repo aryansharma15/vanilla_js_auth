9d3623301d147aa54e3a824765e40d3d83ff981b
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-07-26 22:26:23 +0530

    decide on single page or regular webapp

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ app.use(auth(config));
 app.get("/", (req, res) => {
 	res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
 });
+
+const auth0 = new auth0.WebAuth({
+	domain: "YOUR_AUTH0_DOMAIN",
+	clientID: "YOUR_AUTH0_CLIENT_ID",
+	redirectUri: "http://localhost:YOUR_PORT/callback", // The URL to redirect back to after authentication
+	audience: "https://YOUR_AUTH0_API_IDENTIFIER", // The audience value is optional but may be required for certain setups.
+	responseType: "token id_token",
+	scope: "openid profile", // Adjust the scope as needed for your application
+});
+
+function login() {
+	auth0.authorize;
+}
